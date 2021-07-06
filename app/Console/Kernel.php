5b8e9772adc08e3eb78b11bc04869b5bcc68a03a
Abdelrahman Saeed <abdosaeedelhassan@gmail.com>
2021-07-06 19:51:38 +0200

    updates

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Scrapers\AmazonScraper;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -19,12 +20,45 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+
+            $settings = getSetting([
+                'search_keywords',
+                'search_hours',
+                'search_minutes',
+                'last_search_time'
+            ], null);
+
+            $search_keywords = $settings['search_keywords'];
+            $search_hours = $settings['search_hours'];
+            $search_minutes = $settings['search_minutes'];
+            $last_search_time = $settings['last_search_time'];
+
+            if ($search_keywords && $search_hours && $search_minutes) {
+                $totalTimeInMinutes = ($search_hours * 60) + $search_minutes;
+                $now = new \DateTime();
+                if ($last_search_time) {
+                    $future_date = new \DateTime($last_search_time);
+                } else {
+                    $last_search_time = date('Y-m-d H:i:s');
+                    $future_date = new \DateTime($last_search_time);
+                }
+                $future_date->modify('+' . $totalTimeInMinutes . ' minutes');
+                $formatted_date = $future_date->format('Y-m-d H:i:s');
+                if ($now->format('Y-m-d H:i:s') > $formatted_date) {
+                    AmazonScraper::getSearch($search_keywords);
+                    saveSetting('last_search_time', date('Y-m-d H:i:s'));
+                }
+            }
+
+        })->everyMinute()->timezone(config('app.timezone'));
+
     }
 
     /**
@@ -34,7 +68,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
